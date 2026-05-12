044c2000f8e3c77c0617966cad70e708a1da6895
diegocastro1o <diegoandres.castro@correo.ucu.edu.uy>
2026-05-12 11:33:27 -0300

    Cambios 12/05

--- src/main.py
+++ src/main.py
@@ -3,61 +3,64 @@ from palabras import obtener_palabra
 from random import randint
 
 def normalizar(palabra):
-    palabra = palabra.replace("á", "a")
-    palabra = palabra.replace("é", "e")
-    palabra = palabra.replace("í", "i")
-    palabra = palabra.replace("ó", "o")
-    palabra = palabra.replace("ú", "u")
+    vocales_esp=['á', 'é', 'í', 'ó', 'ú']
+    vocales=['a', 'e', 'i', 'o', 'u']
+    
+    for i in range(len(vocales_esp)):
+        if vocales_esp[i] in palabra:
+            palabra = palabra.replace(vocales_esp[i], vocales[i])        
     
     return palabra
 
 def obtener_cantidad_ocultas(pista):
     cantidad_ocultas = 0
+    
     for letra in pista:
         cantidad_ocultas += 1 if letra == '_' else 0
+        
     return cantidad_ocultas
 
 def agregar_caracter(palabra, index, caracter):
     resultado = ''
+    
     for i in range(len(palabra)):
         resultado += palabra[i] if i != index else caracter
+        
     return resultado
 
 # EJERCICIO 1
-def generar_pista(palabra: str):
+def generar_pista(palabra):
     pista = palabra
+    
     for i in range(len(palabra)):
         cantidad_ocultas = obtener_cantidad_ocultas(pista)
         rand_bin = randint(0, 1)
         if rand_bin == 1 and cantidad_ocultas <= len(palabra) * 0.6:
             pista = agregar_caracter(pista, i, '_')
+    
     return pista
 
 # EJERCICIO 2
 def verificar_palabra(palabra, intento):
-    if normalizar(palabra.lower()) == normalizar(intento.lower()):
-        return True
-    else:
-        return False
+    es_correcto = normalizar(palabra.lower()) == normalizar(intento.lower())
+    
+    return es_correcto
 
 # EJERCICIO 3
 def calcular_puntaje(tiempo_restante, intentos_restantes):
-    puntaje_total=(tiempo_restante*10)+(intentos_restantes*20)
-    return round(puntaje_total, 2)
+    puntaje_total=round((tiempo_restante*10)+(intentos_restantes*20), 2)
+    
+    return puntaje_total
 
 # Ejercicio Adicional
-def actualizar_pista(pista: str, palabra):
+def actualizar_pista(pista, palabra):
     cantidad_ocultas = obtener_cantidad_ocultas(pista)
-    print(f'cantidad ocultas en {pista}', cantidad_ocultas)
-    
     pista_actualizada = pista
     
-    while pista == pista_actualizada:
-        print('inicio while')
+    while pista == pista_actualizada and cantidad_ocultas > 1:
         for i in range(0,len(pista)):
             rand_bin = randint(0, 1)
-            condition = cantidad_ocultas > 1 and rand_bin == 1 and pista[i] == '_'
-            if condition:
+            if rand_bin == 1 and pista[i] == '_':
                 pista_actualizada = agregar_caracter(pista_actualizada, i, palabra[i])
                 break
 
